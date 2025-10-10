661095ec3d09256b65e6ba2513c9f68dc31baa43
Alex-MirandaS <williammiranda201930967@cunoc.edu.gt>
2025-10-10 00:20:15 -0600

    SpringBoot proyect created.

---
+++ reo-comelon-simulator/src/main/java/modela1/reo_comelon_simulator/ReoComelonSimulatorApplication.java
@@ -0,0 +1,14 @@
+package modela1.reo_comelon_simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReoComelonSimulatorApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(ReoComelonSimulatorApplication.class, args);
+		System.out.println("HOLA MUNDO");
+	}
+
+}
---
+++ reo-comelon-simulator/src/test/java/modela1/reo_comelon_simulator/ReoComelonSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package modela1.reo_comelon_simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReoComelonSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
